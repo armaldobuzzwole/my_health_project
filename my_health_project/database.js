200ba1582728fb77bde98ea810d705c1154b129f
cody <cody960829@gmail.com>
2026-06-18 15:33:06 +0800

    Delete my_health_project directory

--- my_health_project/database.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./health.db');
-
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS health_logs (id INTEGER PRIMARY KEY AUTOINCREMENT, log_date DATE NOT NULL, sleep_hours REAL NOT NULL, steps INTEGER NOT NULL, mood_score INTEGER NOT NULL)");
-});
-
-module.exports = db;
